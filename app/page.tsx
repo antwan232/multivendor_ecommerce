26cfb7ca4637b1f09677a26579f4bf71c34fe09a
Tony <Tonynabil300@gmail.com>
2025-11-11 20:08:19 +0200

    feat(layout,nextthemes,shadcnui): basic setup with shadcnUI,ThemeToggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet.
-    </div>
-  );
+	return (
+		<div className="font-barlow">
+			
+		</div>
+	);
 }
---
+++ components/ui/shared/theme-toggle.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+// Icons
+import { MountainIcon, SunIcon } from "lucide-react";
+
+// UI Components
+import { Button } from "../button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../dropdown-menu";
+import { useTheme } from "next-themes";
+
+export default function ThemeToggle() {
+	const { setTheme } = useTheme();
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant="outline"
+					size="icon-lg"
+					className="rounded-full">
+					<SunIcon className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<MountainIcon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+					<span className="sr-only">toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
